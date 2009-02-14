7724982eff29b867d89dd7b23c39c78c0d42f1dc
saleyn <saleyn@6f27c29e-f065-11dd-a320-db7d8e7a638f>
2009-02-14 17:53:09 +0000

    Templatized pipe/tcp servers

--- branches/ei/src/server/server.hpp
+++ branches/ei/src/server/server.hpp
@@ -25,9 +25,15 @@ template<class Request>
 class server : private boost::noncopyable
 {
 public:
+    typedef Request RequestT;
+
     /// Construct the server to listen on the specified TCP address and port, and
     /// serve up files from the given directory.
-    server(const std::string& address, const std::string& port);
+    server()
+        : m_io_service()
+    {}
+
+    virtual ~server() {}
 
     /// Run the server's io_service loop.
     void run() {
@@ -39,79 +45,107 @@ public:
     }
 
     /// Stop the server.
-    void stop() {
+    virtual void stop() {
         // Post a call to the stop function so that server::stop() is safe to call
         // from any thread.
         m_io_service.post(boost::bind(&server::handle_stop, this));
     }
 
-private:
-    /// Handle completion of an asynchronous accept operation.
-    void handle_accept(const boost::system::error_code& e);
+protected:
     /// Handle a request to stop the server.
-    void handle_stop();
+    virtual void handle_stop() {}
     
     /// The io_service used to perform asynchronous operations.
     boost::asio::io_service         m_io_service;
-    /// Acceptor used to listen for incoming connections.
-    boost::asio::ip::tcp::acceptor  m_acceptor;
-    /// The connection manager which owns all live connections.
-    connection_manager<Request>     m_connection_manager;
-    /// The next connection to be accepted.
-    typename connection_manager<Request>::connection_ptr m_new_connection;
     /// The handler for all incoming requests.
     request_handler<Request>        m_request_handler;
 
-    typename Request::AllocatorT    m_allocator;
+    typename RequestT::AllocatorT   m_allocator;
 };
 
-template <class Request>
-server<Request>::server(const std::string& address, const std::string& port)
-    : m_io_service()
-    , m_acceptor(m_io_service)
-    , m_connection_manager()
-    , m_new_connection(
-        new connection<Request>(m_io_service,
-                                m_connection_manager, 
-                                m_request_handler,
-                                m_allocator))
-{
-    // Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
-    boost::asio::ip::tcp::resolver resolver(m_io_service);
-    boost::asio::ip::tcp::resolver::query query(address, port);
-    boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
-    m_acceptor.open(endpoint.protocol());
-    m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-    m_acceptor.bind(endpoint);
-    m_acceptor.listen();
-    m_acceptor.async_accept(m_new_connection->socket(),
-        boost::bind(&server::handle_accept, this, boost::asio::placeholders::error));
-}
 
-template <class Request>
-void server<Request>::handle_accept(const boost::system::error_code& e)
+/// The TCP server.
+template<class Request>
+class tcp_server : public server< Request >
 {
-    if (!e) {
-        m_connection_manager.start(m_new_connection);
-        m_new_connection.reset(
-            new connection<Request>(
+public:
+    typedef server< Request > BaseT;
+
+    /// Construct the server to listen on the specified TCP address and port, and
+    /// serve up files from the given directory.
+    tcp_server(const std::string& address, const std::string& port)
+        : m_connection_manager()
+        , m_acceptor(m_io_service)
+        , m_new_connection(
+            new tcp_connection<Request>(
                 m_io_service, m_connection_manager, 
-                m_request_handler, m_allocator));
+                m_request_handler, m_allocator))
+    {
+        // Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
+        boost::asio::ip::tcp::resolver resolver(m_io_service);
+        boost::asio::ip::tcp::resolver::query query(address, port);
+        boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
+        m_acceptor.open(endpoint.protocol());
+        m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+        m_acceptor.bind(endpoint);
+        m_acceptor.listen();
         m_acceptor.async_accept(m_new_connection->socket(),
-            boost::bind(&server::handle_accept, this, boost::asio::placeholders::error));
+            boost::bind(&tcp_server<Request>::handle_accept, this, boost::asio::placeholders::error));
     }
-}
 
-template <class Request>
-void server<Request>::handle_stop()
+private:
+    /// The connection manager which owns all live connections.
+    connection_manager< tcp_connection<Request> >  m_connection_manager;
+    /// Acceptor used to listen for incoming connections.
+    boost::asio::ip::tcp::acceptor  m_acceptor;
+    /// The next connection to be accepted.
+    typename connection_manager< tcp_connection<Request> >::connection_ptr m_new_connection;
+
+    /// Handle completion of an asynchronous accept operation.
+    void handle_accept(const boost::system::error_code& e) {
+        if (!e) {
+            m_connection_manager.start(m_new_connection);
+            m_new_connection.reset(
+                new tcp_connection<Request>(
+                    m_io_service, m_connection_manager, 
+                    m_request_handler, m_allocator));
+            m_acceptor.async_accept(m_new_connection->socket(),
+                boost::bind(&tcp_server<Request>::handle_accept, this, boost::asio::placeholders::error));
+        }
+    }
+
+    /// Handle a request to stop the server.
+    virtual void handle_stop() {
+        // The server is stopped by cancelling all outstanding asynchronous
+        // operations. Once all operations have finished the io_service::run() call
+        // will exit.
+        m_acceptor.close();
+        m_connection_manager.stop_all();
+    }
+};
+
+
+/// The PIPE server
+template<class Request>
+class pipe_server : public server<Request>
 {
-    // The server is stopped by cancelling all outstanding asynchronous
-    // operations. Once all operations have finished the io_service::run() call
-    // will exit.
-    m_acceptor.close();
-    m_connection_manager.stop_all();
-}
+public:
+    typedef server<Request> BaseT;
 
+    /// Construct the server to listen on the specified TCP address and port, and
+    /// serve up files from the given directory.
+    pipe_server(int fd_in, int fd_out)
+        : m_pipe_connection(m_io_service, m_request_handler, fd_in, fd_out, m_allocator)
+    {
+        m_pipe_connection.start();
+    }
+
+    ~pipe_server() {
+        m_pipe_connection.stop();
+    }
+private:
+    pipe_connection<Request> m_pipe_connection;
+};
 
 } // namespace server
 } // namespace ei
